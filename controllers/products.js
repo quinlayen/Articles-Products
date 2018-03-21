92aea3c05adca07628684f4f9db9a9650bb9535e
quinlayen <quinlayen@gmail.com>
2018-03-20 16:39:38 -1000

    basic templating working.  Directory fleshed out

--- controllers/products.js
+++ controllers/products.js
@@ -2,31 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/products', (req,res)=>{
-    res.send('get products')
+    res.render('./templates/index.hbs', {
+        pageTitle: 'Main Page'
+    })
 })
 
 router.get('/products/:id', (req,res)=>{
-    res.send('get products id')
+    const id = req.params.id;
+    res.render('./templates/products.hbs',{
+        pageTitle: 'Products'
+    })
 })
 
 router.get('/products/:id/edit', (req,res)=>{
     res.send('get products id edit')
 })
 
-router.get('products/new', (req,res)=>{
+router.get('/products/new', (req,res)=>{
     res.send('get products new')
 })
 
-
-router.post('products', (req,res)=>{
+router.post('/products', (req,res)=>{
     res.send('post products')
 })
 
-router.put('products/:id', (req,res)=>{
+router.put('/products/:id', (req,res)=>{
     res.send('put products id')
 })
 
-router.delete('products/:id', (req,res)=>{
+router.delete('/products/:id', (req,res)=>{
     res.send('delete products id')
 })
 
