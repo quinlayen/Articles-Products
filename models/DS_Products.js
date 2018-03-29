382dd9f363d46225046e223d6efa30e90bf73085
quinlayen <quinlayen@gmail.com>
2018-03-28 19:42:15 -1000

    Delete works

--- models/DS_Products.js
+++ models/DS_Products.js
@@ -1,53 +1,54 @@
 class DS_Products {
-    constructor(){
-        this.storage =[];
-        this.idNum = 1;
-        this.initMockProducts();
-    }
-    initMockProducts(){
-        this.storage.push({
-            name: 'Test item 1',
-            price: '$200',
-            inventory: '1',
-            id: this.idNum
-        })
-        this.idNum++;
-        this.storage.push({
-            name: 'Test item 2',
-            price: '$100',
-            inventory: 3,
-            id: this.idNum
-        })
-        this.idNum++
-    }
-    
-    getAllProducts(){
-        return this.storage.slice();
-    }
-    
-    getProductById(id){
-        let result;
-        const filteredId = this.storage.forEach(element =>{
-        if(element.id === id){
-            result = element;
-            return result
-        }  
-    })
-    return result
-}
+  constructor() {
+    this.storage = [];
+    this.idNum = 1;
+    this.initMockProducts();
+  }
+  initMockProducts() {
+    this.storage.push({
+      name: 'Test item 1',
+      price: '$200',
+      inventory: '1',
+      id: this.idNum
+    });
+    this.idNum++;
+    this.storage.push({
+      name: 'Test item 2',
+      price: '$100',
+      inventory: 3,
+      id: this.idNum
+    });
+    this.idNum++;
+  }
 
-createNewItem(newItem){
-    newItem.id = this.idNum++
+  getAllProducts() {
+    return this.storage.slice();
+  }
+
+  getProductById(id) {
+    let result;
+    const filteredId = this.storage.forEach(element => {
+      if (element.id === id) {
+        result = element;
+        return result;
+      }
+    });
+    return result;
+  }
+
+  createNewItem(newItem) {
+    newItem.id = this.idNum++;
     this.storage.push(newItem);
-    return newItem.id
-}
+    return newItem.id;
+  }
 
-changeItemDetails(changeItem){
+  changeItemDetails(changeItem) {
     const itemToSelect = this.getProductById(changeItem);
     console.log('itemToSelect', itemToSelect);
+  }
+  
+  deleteItem(id){
+      return this.storage.splice(this.storage.indexOf(id), 1)
+  }
 }
-
-
-
-}
-module.exports = new DS_Products()
+module.exports = new DS_Products();
